4d2e187e6dcee42431c67a0a6ebc6935cc5353b4
James Hryniw <jhryniw@gmail.com>
2018-01-11 16:42:35 -0700

    extract watchlist checking

--- index.js
+++ index.js
@@ -29,6 +29,38 @@ async function send_email_notification(msg) {
     }
 }
 
+async function check_watchlist(page) {
+    await page.goto(watchlist);
+    const table_rows = await page.$$('[id^="trZSSV_WATCH_LIST$0_row"]');
+
+    const open_classes = [];
+    const closed_classes = [];
+
+    for(const row of table_rows) {
+        const open = await row.$('img[src="/cs/uahebprd/cache2/PS_CS_STATUS_OPEN_ICN_1.gif"]');
+        if(open !== null) {
+            const class_span = await row.$('[id^="win0divDERIVED_REGFRM1_SSR_CLASSNAME_"] span');
+            const class_text = await page.evaluate(span => span.innerHTML, class_span);
+            const section_span = await row.$('[id^="win0divDERIVED_REGFRM1_DESCR40"] span');
+            const section_text = await page.evaluate(span => span.innerHTML, section_span);
+            open_classes.push(class_text+" "+section_text);
+        }
+    }
+
+    for(const row of table_rows) {
+        const open = await row.$('img[src="/cs/uahebprd/cache2/PS_CS_COURSE_ENROLLED_ICN_1.gif"]');
+        if(open !== null) {
+            const class_span = await row.$('[id^="win0divDERIVED_REGFRM1_SSR_CLASSNAME_"] span');
+            const class_text = await page.evaluate(span => span.innerHTML, class_span);
+            const section_span = await row.$('[id^="win0divDERIVED_REGFRM1_DESCR40"] span');
+            const section_text = await page.evaluate(span => span.innerHTML, section_span);
+            closed_classes.push(class_text+" "+section_text);
+        }
+    }
+
+    return [open_classes, closed_classes]
+}
+
 (async() => {
     console.log('launching browser');
     const browser = await puppeteer.launch();
@@ -40,34 +72,9 @@ async function send_email_notification(msg) {
         let prev_availability = "";
 
         while(true) {
-            await page.goto(watchlist);
-            const table_rows = await page.$$('[id^="trZSSV_WATCH_LIST$0_row"]');
 
-            const open_classes = [];
-            const closed_classes = [];
-
-            for(const row of table_rows) {
-                const open = await row.$('img[src="/cs/uahebprd/cache2/PS_CS_STATUS_OPEN_ICN_1.gif"]');
-                if(open !== null) {
-                    const class_span = await row.$('[id^="win0divDERIVED_REGFRM1_SSR_CLASSNAME_"] span');
-                    const class_text = await page.evaluate(span => span.innerHTML, class_span);
-                    const section_span = await row.$('[id^="win0divDERIVED_REGFRM1_DESCR40"] span');
-                    const section_text = await page.evaluate(span => span.innerHTML, section_span);
-                    open_classes.push(class_text+" "+section_text);
-                }
-            }
-
-            for(const row of table_rows) {
-                const open = await row.$('img[src="/cs/uahebprd/cache2/PS_CS_COURSE_ENROLLED_ICN_1.gif"]');
-                if(open !== null) {
-                    const class_span = await row.$('[id^="win0divDERIVED_REGFRM1_SSR_CLASSNAME_"] span');
-                    const class_text = await page.evaluate(span => span.innerHTML, class_span);
-                    const section_span = await row.$('[id^="win0divDERIVED_REGFRM1_DESCR40"] span');
-                    const section_text = await page.evaluate(span => span.innerHTML, section_span);
-                    closed_classes.push(class_text+" "+section_text);
-                }
-            }
-
+            await check_watchlist(page);
+            
             const availability = `
                 <p>
                     open classes: ${open_classes.join(", ")}<br/>
