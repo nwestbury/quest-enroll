74a167ed76b3e02e6f922dfc6e48065cec12d464
Nathan Liebrecht <nwliebrecht@gmail.com>
2018-01-11 13:08:10 -0700

    done emailer

--- README.md
+++ README.md
@@ -1,11 +1,15 @@
 # ualberta-enroll
 
-Create a file called `credentials.json` and fill it in like so:
+Sign up for [mailgun](mailgun.com) and create a file
+called `credentials.json` with this content:
 
 ```
 {
     "username": "your-username",
-    "password": "your-password"
+    "password": "your-password",
+    "mailgun_api_key": "key-[your-unique-key]",
+    "mailgun_domain": "sandbox[your-unique-sandbox-id].mailgun.org",
+    "notify_email": "[your-personal-email]@provider.com"
 }
 ```
 
--- index.js
+++ index.js
@@ -1,20 +1,38 @@
-const puppeteer = require('puppeteer');
 const fs = require('fs');
+const process = require('process');
+const puppeteer = require('puppeteer');
+const mailgunjs = require('mailgun-js');
+
+
 
 const credentials = JSON.parse(fs.readFileSync("credentials.json"));
-const watchlist = "https://www.beartracks.ualberta.ca/psc/uahebprd/EMPLOYEE/HRMS/c/ZSS_STUDENT_CENTER.ZSS_WATCH_LIST.GBL";
 
-const icon_open = "https://www.beartracks.ualberta.ca/cs/uahebprd/cache2/PS_CS_STATUS_OPEN_ICN_1.gif";
-const icon_closed = "https://www.beartracks.ualberta.ca/cs/uahebprd/cache2/PS_CS_COURSE_ENROLLED_ICN_1.gif"
+const mailgun = mailgunjs({apiKey: credentials.mailgun_api_key, domain: credentials.mailgun_domain});
+const watchlist = "https://www.beartracks.ualberta.ca/psc/uahebprd/EMPLOYEE/HRMS/c/ZSS_STUDENT_CENTER.ZSS_WATCH_LIST.GBL";
+const rate = 15*1000;
+
+async function send_email_notification(msg) {
+    const data = {
+        from: 'Watchlist Notifier <postmaster@'+credentials.mailgun_domain+'>',
+        to: credentials.notify_email,
+        subject: 'Bear Tracks Watchlist Notification',
+        text: msg
+    };
+
+    try {
+        const body = await mailgun.messages().send(data);
+        console.log("sent email notification");
+        console.log(body);
+    } catch(e) {
+        console.log("could not send email notification", e);
+    }
+}
 
 async function login(page) {
     console.log('starting login');
     await page.goto(
         'https://www.beartracks.ualberta.ca/uahebprd/signon.html',
         {waitUntil: 'networkidle0'});
-    /*await (await page.$('#signin > a img')).click();
-    console.log('clicked sign in');
-    await page.screenshot({path: 'screenshot.png', fullPage: true});*/
     await page.waitForSelector('#loginform input#username');
     console.log('reached login page');
     await page.type('#loginform input#username', credentials.username);
@@ -33,15 +51,14 @@ async function login(page) {
 
     try {
         await login(page);
+        let prev_availability = "";
 
         while(true) {
             await page.goto(watchlist);
             const table_rows = await page.$$('[id^="trZSSV_WATCH_LIST$0_row"]');
 
-            const open_classes = new Set();
-            const closed_classes = new Set();
-
-            console.log("found ", table_rows.length, "rows");
+            const open_classes = [];
+            const closed_classes = [];
 
             for(const row of table_rows) {
                 const open = await row.$('img[src="/cs/uahebprd/cache2/PS_CS_STATUS_OPEN_ICN_1.gif"]');
@@ -50,7 +67,7 @@ async function login(page) {
                     const class_text = await page.evaluate(span => span.innerHTML, class_span);
                     const section_span = await row.$('[id^="win0divDERIVED_REGFRM1_DESCR40"] span');
                     const section_text = await page.evaluate(span => span.innerHTML, section_span);
-                    open_classes.add(class_text+" "+section_text);
+                    open_classes.push(class_text+" "+section_text);
                 }
             }
 
@@ -61,16 +78,25 @@ async function login(page) {
                     const class_text = await page.evaluate(span => span.innerHTML, class_span);
                     const section_span = await row.$('[id^="win0divDERIVED_REGFRM1_DESCR40"] span');
                     const section_text = await page.evaluate(span => span.innerHTML, section_span);
-                    closed_classes.add(class_text+" "+section_text);
+                    closed_classes.push(class_text+" "+section_text);
                 }
             }
 
-            console.log("open classes: ", [...open_classes].join(", "));
-            console.log("closed classes: ", [...closed_classes].join(", "));
-            console.log("");
-        }
+            const availability = "open classes: " + open_classes.join(", ") +
+                "\nclosed classes: " + closed_classes.join(", ");
 
-        await page.screenshot({path: 'screenshot.png', fullPage: true});
+            if(prev_availability !== availability) {
+                console.log("found ", table_rows.length, "rows");
+                console.log(availability);
+
+                prev_availability = availability;
+                await send_email_notification(availability);
+            } else {
+                process.stdout.write('.');
+            }
+
+            await new Promise(resolve => setTimeout(resolve, rate));
+        }
 
         browser.close();
     } catch(e) {
